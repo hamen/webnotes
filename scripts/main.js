7f3e09a40127cc23857d2ad0c54430c48598d91c
Ivan Morgillo <imorgillo@sanniolug.org>
2010-05-12 23:11:02 +0200

    JQuery added; item submit pressing RET

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@
  
  Author: Ivan Morgillo < imorgillo [at] sannioglug [dot] org >
  */
-function $(id) {
+function _(id) {
     return document.getElementById(id);
 }
 
@@ -25,23 +25,23 @@ var host = location.hostname;
 var myLocalStorage = localStorage; // firefox 3.5+
 
 function writeLocal() {
-  var data = $('text').value;
-  var itemName = $('item_name').value;
+  var data = _('text').value;
+  var itemName = _('item_name').value;
   myLocalStorage.setItem(itemName, data);
   updateItemsList();
 }
 
 function deleteLocal() {
-    var itemName = $('item_name').value;
+    var itemName = _('item_name').value;
     myLocalStorage.removeItem(itemName);
     updateItemsList();
     location.reload(true);
 }
 
 function readLocal(itemName) {
-    $('deleteButton').disabled=false;
-    $('item_name').value=itemName;
-    $('text').value=myLocalStorage.getItem(itemName);
+    _('deleteButton').disabled=false;
+    _('item_name').value=itemName;
+    _('text').value=myLocalStorage.getItem(itemName);
 }
 
 function updateItemsList() {
@@ -55,5 +55,5 @@ function updateItemsList() {
 	  '<span onclick="readLocal(\''+itemName+'\');" title="Click to load"><strong>'+itemName+'</strong></span>'+ 
           '</li>';
   }
-  $('items').innerHTML = s+'</ul>';
+  _('items').innerHTML = s+'</ul>';
 }
