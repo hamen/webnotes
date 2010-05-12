65d0692177ff4850686ac6a5b473171edafb6125
Ivan Morgillo <imorgillo@sanniolug.org>
2010-05-12 21:07:13 +0200

    GPL license added

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
+/*
+ This file is part of 'WebNotes'.
+ 
+ 'WebNotes' is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License,
+ or any later version.
+ 
+ 'WebNotes' is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with 'WebNotes'.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ Author: Ivan Morgillo < imorgillo [at] sannioglug [dot] org >
+ */
 function $(id) {
     return document.getElementById(id);
 }
@@ -29,7 +47,7 @@ function readLocal(itemName) {
 function updateItemsList() {
   var items = myLocalStorage.length;
   // list items
-  var s = '<h2>Items for '+host+'</h2>';
+  var s = '<h2>Stored items:</h2>';
   s+= '<ul>';
   for (var i=0; i < items; i++) {
     var itemName = myLocalStorage.key(i);
