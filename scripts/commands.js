861227115c5fec68fa0b3193518aa66e7a3b6969
Ivan Morgillo <imorgillo@sanniolug.org>
2010-05-15 13:08:53 +0200

    New action key to save a note

--- scripts/commands.js
+++ scripts/commands.js
@@ -2,4 +2,5 @@ function enableCommands() {
     $(document).bind('keydown', {combi: 'a', disableInInput: true}, showTextarea);    
     $(document).bind('keydown', {combi: 'r', disableInInput: true}, resetFields);    
     $(document).bind('keydown', {combi: 'd', disableInInput: true}, deleteWarning);    
+    $(document).bind('keydown', {combi: 'ctrl+return', disableInInput: false}, writeLocal);    
 }
